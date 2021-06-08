969a73df0125951bedea51de47ae86c11276d2bb
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2021-06-07 21:42:10 -0500

    Completed a couple
    Still working on a couple silly ones, but moving forward instead of staying stuck on one for to long.

--- labs109.py
+++ labs109.py
@@ -41,47 +41,94 @@
 #                 return True
 
 
+        ##### Still Working On #####
+# tiles2 = [(3, 5), (5, 2), (2, 3)]
+# tiles = [(1, 3), (3, 2), (2, 5), (5, 2), (2, 1)]
+# def domino_cycle(tiles):
+#         if len(tiles) == 0:
+#                 return True
+#         if len(tiles) == 1:
+#                 return tiles[0][0] == tiles [0][1]      # Returns False
+#         for i in range(len(tiles) - 1):
+#                 if tiles[i][0] == tiles[i + i][0]:
+#                         return True
+#                 return False
 
-tiles2 = [(3, 5), (5, 2), (2, 3)]
-tiles = [(1, 3), (3, 2), (2, 5), (5, 2), (2, 1)]
-
-def domino_cycle(tiles):
-        # print (tiles[1][1])
-        # print(len(tiles) - 1)
-        if len(tiles) == 0:
-                return True
-        if len(tiles) == 1:
-                return tiles[0][0] == tiles [0][1]      # Returns False
-        for i in range(len(tiles) - 1):
-                if tiles[i][0] == tiles[i + i][0]:
-                        return True
-                return False
-
-        # if tiles[i][0] which is the right digit of a tuple, is == to tiles[i + 1][0]
-      
-
-domino_cycle(tiles)
+#         # if tiles[i][0] which is the right digit of a tuple, is == to tiles[i + 1][0]
+# domino_cycle(tiles)
 
 
+        ##### COMPLETE #####
 # def count_dominators(items):
-#         if (items) == []:
-#                 return 0
-#         else: 
-#                 dominators = 1
-#                 for num in range(1, len(items)):
-#                         if items[num] < items[num - 1]:
-#                                 dominators = dominators + 1
-#                         else: 
-#                                 dominators = dominators + 0
-#                         return dominators
+#     dominators = 0
+#     for index,item in enumerate(items):
+#         dominator = True
+#         for right_item in items[index + 1:]:
+#             if item <= right_item:
+#                 dominator = False
+#                 break
+#         if dominator:
+#             dominators = dominators + 1        
+#     return dominators
+# # Runs in 310 seconds in VScode which would run in under 300 seconds locally or in replit likely
+# Runs in 1.572 seconds in replit
+# count_dominators([42, 7, 12, 9, 2, 5])
 
 
-
-# def extract_increasing_integers_from_digit_string
+        ##### Some Reason still working on... #####
 # def extract_increasing(digits):
-#         result = []
-#         converted = str(digits)
-#         for i in converted:
-# extract_increasing(245349)
+#         current = digits[0]
+#         previous = -1
+#         # digits_list = digits.split()
+#         for d in digits:
+#                 print(d)
+# extract_increasing('045349')
 
 
+
+wordlist = open("words_sorted.txt", "r")
+new_words = {}
+
+def words_with_letters(words, letters):
+        for word in words.read().split():
+                if letters in word:
+                        print (word)
+
+
+# words_with_letters(words=wordlist, letters='klore')
+
+
+        ##### COMPLETE #####
+# def taxi_zum_zum(moves):
+#     #creating a list of directions
+#     directions = ['N','E','S','W']
+#     #dir points the current index of directions
+#     dir = 0
+#     #current position
+#     x = 0
+#     y = 0
+#     #looping through each character in moves
+#     for i in moves:
+#         #if character is L, turning left
+#         if i == 'L':
+#             #decrementing dir
+#             dir -= 1
+#             #if dir goes below 0, starting from other side
+#             if dir < 0:
+#                 dir = 3
+#         elif i== 'R':
+#             #doing the same for turning right
+#             dir += 1
+#             if dir > 3:
+#                 dir = 0
+#         elif i == 'F':
+#             if directions[dir]=='N':
+#                 y += 1 #going up/north
+#             elif directions[dir]=='S':
+#                 y -= 1 #going down/south
+#             elif directions[dir]=='E':
+#                 x += 1 #going east/right
+#             elif directions[dir]=='W':
+#                 x -= 1 #going west/left
+#     #returning a tuple containing final position
+#     return (x,y)
